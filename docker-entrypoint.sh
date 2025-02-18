c209628f00d51dbf3c63a27261114f8126340292
KalilovM <mn.kalilov@gmail.com>
2025-02-19 00:14:47 +0100

    Adding migration before building

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+echo "Running Prisma generate..."
+npm run prisma:generate
+
+echo "Deploying Prisma migrations..."
+npm run prisma:deploy
+
+echo "Building the Next.js application..."
 npm run build
+
+echo "Starting the production server..."
 npm run start
