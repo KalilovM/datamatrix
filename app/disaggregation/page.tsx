568d5112b304e13a29ab901008c40f27d592bd9d
KalilovM <mn.kalilov@gmail.com>
2025-03-10 15:10:37 +0100

    fixing issues with build

--- app/disaggregation/page.tsx
+++ app/disaggregation/page.tsx
@@ -1,12 +1,12 @@
 "use server";
 
 import DisaggregationForm from "@/components/disaggregation/DisaggregationForm";
-import MainLayout from "@/components/MainLayout";
+import Layout from "@/shared/ui/Layout";
 
 export default async function Page() {
-  return (
-    <MainLayout>
-      <DisaggregationForm />
-    </MainLayout>
-  );
+	return (
+		<Layout>
+			<DisaggregationForm />
+		</Layout>
+	);
 }
