1a8f4c9e8e54fd27cce9315c24631b029af454d0
Marlen Kalilov <mn.kalilov@gmail.com>
2025-06-13 02:18:37 +0200

    Clear nomenclature when input is empty

--- app/code-links/page.tsx
+++ app/code-links/page.tsx
@@ -5,14 +5,28 @@ import { withRole } from "@/shared/configs/withRole";
 import Layout from "@/shared/ui/Layout";
 import CodeLinksTable from "./CodeLinksTable";
 import LinkedCodesTable from "./LinkedCodesTable";
+import PrintCodes from "@/components/aggregation-codes/PrintCodes";
+import { usePrintTemplate } from "@/nomenclature/hooks/usePrintTemplate";
+import type { NomenclatureOption } from "@/aggregation/model/types";
 
 function CodeLinksPage() {
   const [codes, setCodes] = useState<string[]>([]);
+  const [nomenclature, setNomenclature] = useState<NomenclatureOption | null>(null);
+  const { data: printTemplate } = usePrintTemplate();
 
   return (
     <Layout className="flex-col">
-      <CodeLinksTable onLinkedCodes={setCodes} />
-      <LinkedCodesTable linkedCodes={codes} />
+      <CodeLinksTable
+        onLinkedCodes={setCodes}
+        onNomenclature={setNomenclature}
+      />
+      <LinkedCodesTable linkedCodes={codes} nomenclature={nomenclature} />
+      {printTemplate && (
+        <PrintCodes
+          printTemplate={printTemplate}
+          selectedNomenclature={nomenclature}
+        />
+      )}
     </Layout>
   );
 }
